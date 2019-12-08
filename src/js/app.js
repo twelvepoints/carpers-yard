a947280a5711e68ec1e1cea93f9ba0a8a6b1a673
Dave Perry <dave@Daves-MacBook-Pro.local>
2019-12-08 02:11:34 +0000

    first commit - pre npm i

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+const swiperFeatured = new Swiper('.featured-carousel .swiper-container', {
+    updateOnWindowResize: true,
+    roundLengths: true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    spaceBetween: 8,
+    slidesPerView: 1.15,
+    breakpoints: {
+        1024: {
+            slidesPerView: 2.5,
+            pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
+            clickable: true
+        }
+    }
+});
+
+const swiperCard = new Swiper('.card-carousel .swiper-container', {
+    updateOnWindowResize: true,
+    roundLengths: true,
+    grabCursor: true,
+    freeModeMomentum: true,
+    spaceBetween: 8,
+    slidesPerView: 2.5,
+    breakpoints: {
+        1024: {
+            slidesPerView: 5.5,
+            pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
+            clickable: true
+        }
+    }
+});
